385a66bd2e737c0964c8ea4dc79e2cce0252b7e8
Stéphane Traissac <Stephane.Traissac@ecofog.gf>
2018-05-04 15:56:14 -0300

    fuzzy double

--- R/f_Double.R
+++ R/f_Double.R
@@ -12,5 +12,37 @@
 #' @examples
 #' Double(runif(3))
 Double<-function(number){
-  return(2*number)
+  # Input check
+  if (!is.numeric(number))
+    stop("Double requires a numeric object")
+  return(2L*number)
+}
+
+
+#' FuzzyDouble
+#'
+#' Fuzzy double of a numeric object.
+#'
+#' Doubles an object with a random noise: a Gaussian error drawn
+#' by \code{\link{rnorm}}.
+#'
+#' @param x A numeric object
+#' @param mean The mean noise. Default is 0.
+#' @param sd The standard deviation of the noise. Default is 1.
+#'
+#' @return a \code{FuzzyDouble} object which is a data.frame with
+#' columns \code{x} for the input and \code{y} for the output.
+#'
+#' @seealso \code{\link{plot.FuzzyDouble}},
+#' \code{\link{autoplot.FuzzyDouble}}
+#' @export
+FuzzyDouble <- function(x, mean = 0, sd = 1) {
+  # Double x and add normal error
+  y <- 2 * x + stats::rnorm(n = length(x), mean = mean,
+                            sd = sd)
+  # Make a data.frame
+  fuzzydouble <- data.frame(x = x, y = y)
+  # Make it a FuzzyMultiple object
+  class(fuzzydouble) <- c("FuzzyDouble", class(fuzzydouble))
+  return(fuzzydouble)
 }
